01d593dafbf28520f535d19a5b15be7e2fbb5f66
Tony <tonythecoder@outlook.com>
2022-12-11 14:38:34 -0500

    Another quick review of Node.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", function (req, res) {
+    res.send("<h1>Hello, world</h1>")
+});
+
+app.get("/contact", function (req, res) {
+    res.send("Contact me at a@a.com");
+});
+
+app.get("/about", function (req, res) {
+    res.send("Stuff about me")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.get("/hobbies", function (req, res) {
+    res.send("<ul><li>Work</li><li>Reading</li><li>Learning</li></ul>")
+});
+app.get("/hobbies", function (req, res) {
+    res.send("<ul><li>Work</li><li>Reading</li><li>Learning</li></ul>")
+});
+app.listen(3000, function () {
+    console.log("Server Started on Port 3000")
+});
 
